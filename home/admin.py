f0cce918db4c61ca083ba816b690de5c8c8cc592
Gaurav kadaskar <111632483+GAURAVKADASKAR@users.noreply.github.com>
2024-03-18 11:18:04 +0530

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from home.models import *
+
+admin.site.register(registration)
+admin.site.register(lo)
+admin.site.register(patient_info)
+admin.site.register(info)
