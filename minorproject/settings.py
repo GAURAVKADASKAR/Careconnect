360b9942a4496313611faa35c12f26475d26516d
new changes <kadskargaurav@gmail.com>
2024-05-22 10:45:44 +0530

    vhanges

--- minorproject/settings.py
+++ minorproject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'django'
 ]
 
 MIDDLEWARE = [
@@ -141,7 +142,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
