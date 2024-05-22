bdf7ceed09b824e46a0903f97a7856a0cca9ceb8
new changes <kadskargaurav@gmail.com>
2024-05-22 10:22:30 +0530

    changes:

--- minorproject/settings.py
+++ minorproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t8g0iz=#f77v-69d-%22a%r@_&857wu6oena_gpd!5@h*qh=s4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
