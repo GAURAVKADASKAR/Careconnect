1840038077989416bfeaa23d289c41b020f22724
new changes <kadskargaurav@gmail.com>
2024-05-22 15:26:30 +0530

    chnages

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,27 @@
 echo " BUILD START"
 python3 -m venv env
 source env/bin/activate
+#!/bin/bash
+# Install dependencies
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Making the migrations..."
+python manage.py makemigrations
+# Apply migrations
+echo "Applying migrations..."
+python manage.py migrate
+
+# Create superuser (optional)
+echo "Creating superuser (optional)..."
+python manage.py createsuperuser --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+echo "Build process completed."
+
 pip install -r requirements.txt
 
 python3 manage.py collectstatic --noinput --clear
