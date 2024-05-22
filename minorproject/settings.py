e7c883b664f914359cf919833f713d54ca6aa934
new changes <kadskargaurav@gmail.com>
2024-05-22 15:11:50 +0530

    changes

--- minorproject/settings.py
+++ minorproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'minorproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'home/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'home/template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
